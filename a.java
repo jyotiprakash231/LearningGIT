6115dc39e37f3cde33691228e974bd7e49487084
Jyoti Prakash Sahoo <47482490+jyotiprakash231@users.noreply.github.com>
2019-10-07 12:27:19 +0530

    sample java file

---
+++ a.java
@@ -0,0 +1,16 @@
+public class a{
+public static void main(String args[]){
+int num=5;
+for(int i=1;i<=num;i++){
+for(int j=1;j<=i;j++){
+
+if(i%2!=0)
+System.out.print("*");
+else
+System.out.println();
+
+}
+
+}
+}
+}
